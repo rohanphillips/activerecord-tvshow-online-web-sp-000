747aa26f5f644fe98843a97bbe809815f5b7dab6
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-23 18:36:49 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,7 +2,10 @@
 class CreateShows < ActiveRecord::[5.2]
   def change
     create_table :shows do |t|
-
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
     end
   end
 end
